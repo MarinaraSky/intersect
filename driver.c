0a351c7c10c700442d9304746f431df5a8d1bfe3
Jack Spence <jack.c.spence@gmail.com>
2018-10-06 00:07:55 -0400

    Ran source files though indent.

--- driver.c
+++ driver.c
@@ -2,18 +2,21 @@
 #include "intersect_bst.h"
 
 
-int main(int argc, char *argv[])
+int
+main(
+    int argc,
+    char *argv[])
 {
-    if(argc > 2)
+    if (argc > 2)
     {
-		int files_opened = 0;
-        Node *tree = getWords(&files_opened, argc, argv);
-        printTree(tree, files_opened);
-		destroyTree(tree);
-    }
-	else
-	{
-		printf("Usage: %s filename1 filename2 [filename 3 ...]\n", argv[0]);
-	}
-}
+        int             files_opened = 0;
+        Node           *tree = getWords(&files_opened, argc, argv);
 
+        printTree(tree, files_opened);
+        destroyTree(tree);
+    }
+    else
+    {
+        printf("Usage: %s filename1 filename2 [filename 3 ...]\n", argv[0]);
+    }
+}
--- intersect_bst.h
+++ intersect_bst.h
@@ -9,15 +9,30 @@ typedef struct Node Node;
 /* Taking a list of arguments, opens the first file and creates
  * a BST. Subsequent files are compared to the BST and count is 
  * updated if appropriate */
-Node *getWords(int *files_opened, int argc, char *argv[]);
+Node           *getWords(
+    int *files_opened,
+    int argc,
+    char *argv[]);
+
 /* Takes a word and a buffer for a temp variable to use for
  * removing leading and trailing punctuation */
-int ignorePuncCmp(const char *tree_word, const char *word);
+int             ignorePuncCmp(
+    const char *tree_word,
+    const char *word);
+
 /* Function that does the stripping of punctuation for ignorePuncCmp */
-void stripPunct(const char *word, char *tmp, bool *symbols);
+void            stripPunct(
+    const char *word,
+    char *tmp,
+    bool * symbols);
+
 /* Generic BST Print function */
-void printTree(Node *tree, int argc);
+void            printTree(
+    Node * tree,
+    int argc);
+
 /* Generic BST Destroy function */
-void destroyTree(Node *tree);
+void            destroyTree(
+    Node * tree);
 
 #endif
